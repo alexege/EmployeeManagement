5d06e5eff0a96bc08aae6da2757c920fafd06b89
Alexander Ege <alexander@ege.com>
2019-12-02 11:25:00 -0800

    Basic MVC Structure complete. With Validations

---
+++ EmployeeManagement/Controllers/EmployeesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            Employee employee = new Employee()
+            {
+                EmployeeId = 1,
+                Name = "Alexander Ege",
+                Email = "Alexander@Ege.com",
+                Gender = "M",
+                RegistrationDate = DateTime.Now,
+                SelectedDays = "Day1, Day2",
+                Request = "N/A"
+            };
+
+            return View(employee);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                //Logic to add Employee to Database
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+    }
+}
---
+++ EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        [Display(Name = "No")]
+        public int EmployeeId { get; set; }
+
+        [StringLength(maximumLength: 30, ErrorMessage = "Name cannot exceed 30 characters")]
+        [Required]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+
+        public string Gender { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/mm/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Date Reg")]
+        [Required]
+        public DateTime RegistrationDate { get; set; }
+
+        [Display(Name = "Selected Days")]
+        [Required]
+        public string SelectedDays { get; set; }
+
+        [Display(Name = "Add. request")]
+        [StringLength(100)]
+        public string Request { get; set; }
+    }
+}
